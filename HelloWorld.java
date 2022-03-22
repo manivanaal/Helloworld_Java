d3e45ef6cb06967eec352170b43f806e9a2207d4
manivanaal <80744015+manivanaal@users.noreply.github.com>
2022-03-23 01:05:49 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Mani");
-    System.out.println("Mani");
-    System.out.println("Mani");
-    System.out.println("Mani");
-    System.out.println("Mani");
-    System.out.println("Mani");
+    System.out.println("swathi");
   }
 }
